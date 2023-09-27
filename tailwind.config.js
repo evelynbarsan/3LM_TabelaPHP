7bafa212d4139cf88fffbe39f132e6d93180c84d
Evelyn Santos <117581658+eveffc@users.noreply.github.com>
2023-09-27 13:22:42 -0300

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        roxo:'#6701a6',
+        violeta:'#8b10ae',
+        cinzaClaro:'#d8d4d4',
+        cinza:'#9b9b9b',
+        grafite:'#4d4e50',
+      },
+    },
   },
   plugins: [],
 }
